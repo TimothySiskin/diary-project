cfa610268bbdeb208ff6cee4b56cf6de20253274
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-04-05 17:40:06 +0200

    +lodash +/posts/:postName rout

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { data: homeStartingContent, posts });
+  res.render("home", { data: homeStartingContent, posts, _ });
 });
 
 app.get("/about", (req, res) => {
@@ -42,6 +43,19 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 });
 
+app.get("/posts/:postName", (req, res) => {
+  let postName = req.params.postName;
+  for (let post of posts) {
+    let title = _.replace(_.lowerCase(post.title), " ", "-");
+    if (title === postName) {
+      console.log("im working!");
+      res.render("post", { post }).end();
+    }
+  }
+  console.log("I'm working too!");
+  res.redirect("/");
+});
+
 app.listen(4500, function () {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 4500");
 });
